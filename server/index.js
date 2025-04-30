9ba7bac489e3f3a93dc92e7cb08c4acef237eac0
Munna-Prasad-Gupta <munnarauniyar789@gmail.com>
2025-04-30 18:17:35 +0530

    Prepare for deplo

--- server/index.js
+++ server/index.js
@@ -81,7 +81,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://myo-dx-sde-wdt6.vercel.app/',
+  origin: 'https://myo-dx-sde-wdt6.vercel.app',
   credentials: true
 }));
 app.use(express.json({ limit: "50mb" }));
