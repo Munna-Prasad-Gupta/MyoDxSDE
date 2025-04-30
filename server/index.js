cb83067a3023963fe4dfa68a6ff1b6f2e3fa3e05
Munna-Prasad-Gupta <munnarauniyar789@gmail.com>
2025-04-30 18:01:22 +0530

    Prepare for deplo

--- server/index.js
+++ server/index.js
@@ -81,7 +81,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://myodxsdebymunnaprasad.onrender.com',
+  origin: 'https://myo-dx-sde-wdt6.vercel.app/',
   credentials: true
 }));
 app.use(express.json({ limit: "50mb" }));
